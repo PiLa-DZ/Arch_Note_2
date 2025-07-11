b245f1dd1b8617e8b3f11438facb185819199728
Nabil <Nabil@gmail.com>
2025-07-11 20:10:30 +0100

    Fri Jul 11 08:10:30 PM CET 2025

--- Manual_Setup/1_Github.sh
+++ Manual_Setup/1_Github.sh
@@ -1,5 +1,11 @@
 cd; ssh-keygen
-mkdir MyGithub
-cd MyGithub
+mkdir ~/MyGithub
+cd ~/MyGithub
 git clone git@github.com:PiLa-DZ/Arch_Note_2.git
+git clone git@github.com:PiLa-DZ/Note_Book.git
+git clone git@github.com:PiLa-DZ/Image.git
+git clone git@github.com:PiLa-DZ/JavaScript_NoteBook.git
+git clone git@github.com:PiLa-DZ/Node_Course.git
+git clone git@github.com:PiLa-DZ/test.git
+git clone 
 mv Arch_Note_2 Arch_Note
--- Manual_Setup/2_Wallpaper.sh
+++ Manual_Setup/2_Wallpaper.sh
@@ -1,3 +1,2 @@
 cd ~/MyGithub
-git clone git@github.com:PiLa-DZ/Image.git
 cp ~/MyGithub/Image/Wallpaper ~/.config
