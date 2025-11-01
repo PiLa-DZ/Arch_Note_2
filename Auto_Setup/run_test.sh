cc2cb3c364f7bba4c6f29e96fa7c6c1fc01aaae8
Nabil <Nabil@gmail.com>
2025-11-01 20:20:33 +0100

    Sat Nov  1 08:20:33 PM CET 2025

--- Auto_Setup/run_test.sh
+++ Auto_Setup/run_test.sh
@@ -1,53 +1,52 @@
 #!/bin/bash
 
 # // Xprofile Config ==============================
-cp ~/MyGithub/Arch_Note/Auto_Setup/.xprofile ~/.xprofile
+cp ~/00_Github/Arch_Note/Auto_Setup/.xprofile ~/.xprofile
 echo "= Copy .xprofile =====================> ok <="
 
-
 # // Kitty Config =================================
-cp -r ~/MyGithub/Arch_Note/Auto_Setup/kitty ~/.config
+cp -r ~/00_Github/Arch_Note/Auto_Setup/kitty ~/.config
 
 # // i3 Confog ====================================
-cp -r ~/MyGithub/Arch_Note/Auto_Setup/i3 ~/.config
+cp -r ~/00_Github/Arch_Note/Auto_Setup/i3 ~/.config
 
 # // Polybar Config ===============================
-cp -r ~/MyGithub/Arch_Note/Auto_Setup/polybar ~/.config
+cp -r ~/00_Github/Arch_Note/Auto_Setup/polybar ~/.config
 
 # // Picom Config =================================
-cp -r ~/MyGithub/Arch_Note/Auto_Setup/picom ~/.config
+cp -r ~/00_Github/Arch_Note/Auto_Setup/picom ~/.config
 
 # // Rofi Config ==================================
-cp -r ~/MyGithub/Arch_Note/Auto_Setup/rofi ~/.config
+cp -r ~/00_Github/Arch_Note/Auto_Setup/rofi ~/.config
 
 # // Bachrc Config ================================
 if grep -R "#--> From Auto_Setup" ~/.bashrc >/dev/null; then
   echo "" >/dev/null
 else
-  cat ~/MyGithub/Arch_Note/Auto_Setup/.bashrc >>~/.bashrc
+  cat ~/00_Github/Arch_Note/Auto_Setup/.bashrc >>~/.bashrc
 fi
 
 # // Bach_Profile Config ==========================
 if grep -R "#--> From Auto_Setup" ~/.bash_profile >/dev/null; then
   echo "" >/dev/null
 else
-  cat ~/MyGithub/Arch_Note/Auto_Setup/.bash_profile >>~/.bash_profile
+  cat ~/00_Github/Arch_Note/Auto_Setup/.bash_profile >>~/.bash_profile
 fi
 
 # // Git Config ===================================
-~/MyGithub/Arch_Note/Auto_Setup/Git_Config.sh
+~/00_Github/Arch_Note/Auto_Setup/Git_Config.sh
 
 # // My Scripts ===================================
-cp -r ~/MyGithub/Arch_Note/Auto_Setup/bin ~/.local
+cp -r ~/00_Github/Arch_Note/Auto_Setup/bin ~/.local
 echo "= Copy My Scripts ====================> ok <="
 
 # // Notifications ================================
-cp -r ~/MyGithub/Arch_Note/Auto_Setup/dunst ~/.config
+cp -r ~/00_Github/Arch_Note/Auto_Setup/dunst ~/.config
 echo "= Notifications ======================> ok <="
 
 # // Vim ==========================================
-cp -r ~/MyGithub/Arch_Note/Auto_Setup/.vimrc ~/
-cp -r ~/MyGithub/Arch_Note/Auto_Setup/.vim ~/.vim
+cp -r ~/00_Github/Arch_Note/Auto_Setup/.vimrc ~/
+cp -r ~/00_Github/Arch_Note/Auto_Setup/.vim ~/.vim
 echo "= Vim ================================> ok <="
 
 # // Chnge Default Image Viewer ===================
